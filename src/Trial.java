5960cbd92143f5a2057b59ad4bf6def899c6a5e4
Yuriy Poltaev <yuriy.poltaev@gmail.com>
2020-06-16 11:53:24 +0400

    update first java class Trial.java

--- src/Trial.java
+++ src/Trial.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class Trial {
+    public static void main(String[] args) {
+        int[] massive = {6, 1, 3, 2, 8, 10, 1, 0};
+        for (int i = 0; i < massive.length - 1; i++) {
+            for (int j = 0; j <= massive.length-2; j++) {
+                if (massive[j] > massive[j + 1]) {
+                    int tmp = massive[j];
+                    massive[j] = massive[j + 1];
+                    massive[j + 1] = tmp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(massive));
+    }
 }
+
